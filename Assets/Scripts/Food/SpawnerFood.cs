7e539cafaea50858abec4251fb7682d9bebb3a1b
liuchristophe <liu3@hotmail.fr>
2021-02-05 17:21:39 +0100

    Pluie de nourriture

---
+++ Assets/Scripts/Food/SpawnerFood.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerFood : MonoBehaviour
+{
+	public GameObject[] foodPrefab = new GameObject[2];
+	//public GameObject foodPrefab;
+	public Transform spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {	
+    	if(Input.GetKey("g")){
+        	foodCreate();
+    	}
+    }
+
+
+
+    void foodCreate(){
+    	float number = Random.Range(-9.3f,9.3f);
+        Instantiate(foodPrefab[Random.Range(0,2)], spawn.position + new Vector3(number, 0, 0), spawn.rotation);
+    }
+}
